5e5202074791c976c9ecc3d7d3161f6f74f3813b
claraharguindey <clarisaharguindey@gmail.com>
2025-03-05 19:44:59 +0100

    start

---
+++ script.js
@@ -0,0 +1,20 @@
+const params = new URLSearchParams(window.location.search);
+const acceso = params.get("name");
+
+const contenidos = {
+    "maria": {  name: "de María Belen" },
+    "estela": { name: "de Estela" },
+    "sonia": { name: "de Sonia" },
+    "anaisabel": { name: "de Ana Isabel" },
+    "cristinaf": { name: "de Cristina Fernández" },
+    "cristinab": { name: "de Cristina Blanco" },
+    "daniel": { name: "de Daniel" },
+    "clara": { name: "de Clara" },
+    "laura": { name: "de Laura" },
+};
+
+if (contenidos[acceso]) {
+    document.getElementById("name").innerText = contenidos[acceso].name;
+} else {
+    document.getElementById("name").innerText = "";
+}
