a79b1ab2183d512b38029c10110d0f6e32a58ad2
claraharguindey <clarisaharguindey@gmail.com>
2025-04-22 16:41:59 +0200

    Add names and images

---
+++ elcano/2/1/script.js
@@ -0,0 +1,61 @@
+const normalizeString = (str) => {
+  return str
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s+/g, "");
+};
+
+const params = new URLSearchParams(window.location.search);
+let acceso = params.get("name");
+
+if (acceso) {
+  acceso = normalizeString(acceso);
+}
+
+const contenidos = {
+  munir: { name: "de Munir", color: "#F4A460" }, // Marrón arena
+  ianIsmael: { name: "de Ian Ismael", color: "#8FBC8F" }, // Verde mar oscuro
+  yanis: { name: "de Yanis", color: "#E6E6FA" }, // Lavanda claro
+  valentina: { name: "de Valentina", color: "#FF69B4" }, // Rosa fuerte
+  lizzie: { name: "de Lizzie", color: "#BC8F8F" }, // Rosa indio
+  kixsi: { name: "de Kixsi", color: "#9370DB" }, // Púrpura medio
+  naia: { name: "de Naia", color: "#98FB98" }, // Verde claro
+  teo: { name: "de Teo", color: "#40E0D0" }, // Turquesa
+  david: { name: "de David", color: "#4169E1" }, // Azul real
+  mireya: { name: "de Mireya", color: "#FFB347" }, // Naranja pastel
+  harsirat: { name: "de Harsirat", color: "#C0C0C0" }, // Plateado
+  elena: { name: "de Elena", color: "#FF7F50" }, // Coral
+  lilia: { name: "de Lilia", color: "#F5DEB3" }, // Trigo
+  sophia: { name: "de Sophia", color: "#EE82EE" }, // Violeta
+  alejandra: { name: "de Alejandra", color: "#FF6347" }, // Rojo tomate
+  lucas: { name: "de Lucas", color: "#FF8C00" }, // Naranja oscuro (ya usado)
+  sidi: { name: "de Sidi", color: "#A9A9A9" }, // Gris oscuro
+  santiago: { name: "de Santiago", color: "#BA55D3" }, // Orquídea medio (ya usado)
+  dulce: { name: "de Dulce", color: "#FFE4E1" }, // Rosa neblina
+  chloe: { name: "de Chloe", color: "#6A5ACD" }, // Azul pizarra
+  nicolas: { name: "de Nicolás", color: "#00FF7F" }, // Verde primavera
+  naomi: { name: "de Naomi", color: "#FAFAD2" }, // Amarillo claro
+  antonella: { name: "de Antonella", color: "#FFC0CB" }, // Rosa
+  ianAlessandro: { name: "de Ian Alessandro", color: "#BDB76B" }, // Caqui oscuro
+  miguel: { name: "de Miguel", color: "#00BFFF" },
+  abigail: { name: "de Abigail", color: "#00BFFF" }, // Azul profundo
+};
+
+const claveEncontrada = Object.keys(contenidos).find(
+  (key) => normalizeString(key) === acceso
+);
+
+if (claveEncontrada) {
+  document.getElementById("name").innerText = contenidos[claveEncontrada].name;
+  document.getElementById("main").style.backgroundColor =
+    contenidos[claveEncontrada].color;
+  document.getElementById("image1").style.display = "none";
+  document.getElementById("image2").src = `./drawings/${claveEncontrada}.jpeg`;
+  document.getElementById(
+    "image2"
+  ).alt = `El poder de ${contenidos[claveEncontrada].name}`;
+  document.getElementById("image2").style.filter = "invert(0)";
+} else {
+  document.getElementById("name").innerText = acceso ? `de ${acceso}` : "";
+}
---
+++ elcano/2/2/script.js
@@ -0,0 +1,63 @@
+const normalizeString = (str) => {
+  return str
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s+/g, "");
+};
+
+const params = new URLSearchParams(window.location.search);
+let acceso = params.get("name");
+
+if (acceso) {
+  acceso = normalizeString(acceso);
+}
+
+const contenidos = {
+  ashley: { name: "de Ashley", color: "#FFB6C1" }, // Rosa claro
+  rita: { name: "de Rita", color: "#ADD8E6" }, // Azul claro
+  nathaly: { name: "de Nathaly", color: "#20B2AA" }, // Verde azulado
+  jadel: { name: "de Jadel", color: "#D8BFD8" }, // Lavanda
+  anas: { name: "de Anas", color: "#FFA07A" }, // Salmón claro
+  emma: { name: "de Emma", color: "#66CDAA" }, // Verde mediano
+  santiago: { name: "de Santiago", color: "#BA55D3" }, // Orquídea medio
+  raider: { name: "de Raider", color: "#CD5C5C" }, // Rojo indio
+  saffwana: { name: "de Saffwana", color: "#F0E68C" }, // Caqui claro
+  dafne: { name: "de Dafne", color: "#E9967A" }, // Salmón oscuro
+  sheyla: { name: "de Sheyla", color: "#AFEEEE" }, // Azul pálido
+  anran: { name: "de Anran", color: "#DAA520" }, // Dorado oscuro
+  barae: { name: "de Barae", color: "#87CEFA" }, // Azul cielo claro
+  jose: { name: "de José", color: "#C71585" }, // Rosa fuerte oscuro
+  mehjabin: { name: "de Mehjabin", color: "#5F9EA0" }, // Verde cadete
+  gabriela: { name: "de Gabriela", color: "#FA8072" }, // Salmón
+  jorgan: { name: "de Jorgan", color: "#A0522D" }, // Marrón rojizo
+  lucas: { name: "de Lucas", color: "#FF8C00" }, // Naranja oscuro
+  alan: { name: "de Alan", color: "#7B68EE" }, // Azul pizarra medio
+  katalina: { name: "de Katalina", color: "#FFDAB9" }, // Melocotón
+  abigail: { name: "de Abigail", color: "#00CED1" }, // Turquesa oscuro
+  ivan: { name: "de Iván", color: "#DC143C" }, // Carmesí
+  marcos: { name: "de Marcos", color: "#00FA9A" }, // Verde primavera medio
+  noemi: { name: "de Noemí", color: "#B22222" }, // Rojo ladrillo
+  alaia: { name: "de Alaia", color: "#DB7093" }, // Rosa pálido
+  vanessa: { name: "de Vanessa", color: "#4682B4" }, // Azul acero
+  sarayma: { name: "de Sarayma", color: "#9ACD32" }, // Verde amarillento
+};
+
+
+const claveEncontrada = Object.keys(contenidos).find(
+  (key) => normalizeString(key) === acceso
+);
+
+if (claveEncontrada) {
+  document.getElementById("name").innerText = contenidos[claveEncontrada].name;
+  document.getElementById("main").style.backgroundColor =
+    contenidos[claveEncontrada].color;
+  document.getElementById("image1").style.display = "none";
+  document.getElementById("image2").src = `./drawings/${claveEncontrada}.jpeg`;
+  document.getElementById(
+    "image2"
+  ).alt = `El poder de ${contenidos[claveEncontrada].name}`;
+  document.getElementById("image2").style.filter = "invert(0)";
+} else {
+  document.getElementById("name").innerText = acceso ? `de ${acceso}` : "";
+}
